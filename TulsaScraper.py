c5ee7ea6beea566a4415e478f3577d857d7725f7
apologet1c <42360331+apologet1c@users.noreply.github.com>
2021-09-07 18:30:53 -0500

    Update TulsaScraper.py
    Updates!

--- TulsaScraper.py
+++ TulsaScraper.py
@@ -1,19 +1,23 @@
 import mechanize
 import urllib.request
 import pandas as pd
-from textwrap import wrap
 import string
 import re
 import time
+import os
+import requests
+import boto3
+from PIL import Image, ImageSequence
 
-pd.options.mode.chained_assignment = None  # default='warn'
+#can't remember what this does
+pd.options.mode.chained_assignment = None
 
 br = mechanize.Browser()
 br.set_handle_robots(False)   # ignore robots.txt
-br.set_handle_refresh(False)  # can sometimes hang without this per what someone on the internet says
+br.set_handle_refresh(False)  # can sometimes hang without this
 
 #get the webpage
-page = br.open('https://www.oscn.net/applications/oscn/report.asp?report=WebJudicialDocketJudgeCaseTypeAll&errorcheck=true&Judge=1012&database=&db=Tulsa&CaseTypeID=26&StartDate=08%2F23%2F2021&GeneralNumber=1&generalnumber1=1&GeneralCheck=on')
+page = br.open('https://www.oscn.net/applications/oscn/report.asp?report=WebJudicialDocketJudgeCaseTypeAll&errorcheck=true&Judge=1012&database=&db=Tulsa&CaseTypeID=26&StartDate=09%2F08%2F2021&GeneralNumber=1&generalnumber1=1&GeneralCheck=on')
 html = page.read().decode("utf-8")
 
 #now find each docket # and format it into an excel link
@@ -39,18 +43,16 @@ for i in urls:
     #generate file links
     caseID= i.replace('https://www.oscn.net/applications/oscn/GetCaseInformation.asp?submitted=true&db=Tulsa&casemasterid=', '')
     barcodes = re.findall("barcode=\d+", html)
-    print(barcodes)
     
     #what if we don't find any documents?
-    if len(barcodes) is 0:
+    if len(barcodes) == 0:
         print("zero")
         petitions.append("NONE")
         cares.append("NONE")
 
-    if len(barcodes) is 1:
+    if len(barcodes) == 1:
         petitioncode = barcodes[0]
         carescode = "NONE"
-        print("one")
         petitions.append("https://www.oscn.net/applications/oscn/getimage.tif?submitted=true&casemasterid=" + caseID + "&db=TULSA&" + petitioncode)
         cares.append("NONE")
 
@@ -60,18 +62,74 @@ for i in urls:
         petitions.append("https://www.oscn.net/applications/oscn/getimage.tif?submitted=true&casemasterid=" + caseID + "&db=TULSA&" + petitioncode)
         cares.append("https://www.oscn.net/applications/oscn/getimage.tif?submitted=true&casemasterid=" + caseID + "&db=TULSA&" + carescode)
 
-    time.sleep(1)
-
 excelpets = ["=HYPERLINK(\"" + e + "\", \"Petition\")" for e in petitions]
 excelcares = ["=HYPERLINK(\"" + e + "\", \"Cares Verification\")" for e in cares]
 
-#send to dataframe and export to csv
+print("done!")
+
+os.chdir("C:/Users/AnthonySeverin/TIFs")
+count = 1
+
+for i in petitions:  
+    #pulls down tif file from OSCN
+    r = requests.get(i, allow_redirects=True)
+    filename = str(count) + ".tif"
+    newfilename = str(count) + ".png"
+    open(filename, 'wb').write(r.content)
+    
+    #conversion goes here
+    im = Image.open(filename)
+    for i, page in enumerate(ImageSequence.Iterator(im)): #kernel crashes unless this is in a for loop for some ungodly reason
+        page.save(newfilename)
+        break
+    #increment counter for filenames
+    count = count + 1
+    
+print("done")
+
+
+#now we call the textract wrapper and initialize address lists
+client = boto3.client('textract')
+address1list = []
+address2list = []
+
+
+#open each image and read it
+while i <= count:
+    count = count - 1
+    filename = str(count) + ".png"
+    with open(filename, 'rb') as document:
+        img = bytearray(document.read())
+
+    #call textract
+    response = client.detect_document_text(Document={'Bytes': img})
+
+    #Textract returns one line at a time, so we'll join it all into a single string
+    text = ""
+    for item in response["Blocks"]:
+        if item["BlockType"] == "LINE":
+            text += "\n" + item["Text"]
+
+    #use regex to pull out addresses
+    address1 = re.findall("(?<=resides at ).*?(=%d{5})", text)
+    address2 = re.findall("(?<=address is ).*?(=%d{5})", text)
+    
+    print(count)
+    print(address1)
+    print(address2)
+    print(text)
+    
+    #append to addresslists
+    address1list.append(address1)
+    address2list.append(address2)
+    
+    #send to dataframes so pandas can export to csv
 df = pd.DataFrame()
 df["Docket Number"] = dockets
 df["Docket Links"] = excellinks
 df["Petition"] = excelpets
 df["Cares Act Affidavit"] = excelcares
+#df["ResAdd"] = address1list
+#df["MailAdd"] = address2list
 
 df.to_csv('evictions.csv', index=False)
-
-print("done!")
