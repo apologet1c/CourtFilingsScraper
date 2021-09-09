5206169f6a93398690fd09517f817b4937db568e
apologet1c <42360331+apologet1c@users.noreply.github.com>
2021-09-09 11:04:45 -0500

    Update TulsaScraper.py

--- TulsaScraper.py
+++ TulsaScraper.py
@@ -17,7 +17,7 @@ br.set_handle_robots(False)   # ignore robots.txt
 br.set_handle_refresh(False)  # can sometimes hang without this
 
 #get the webpage
-page = br.open('https://www.oscn.net/applications/oscn/report.asp?report=WebJudicialDocketJudgeCaseTypeAll&errorcheck=true&Judge=1012&database=&db=Tulsa&CaseTypeID=26&StartDate=09%2F08%2F2021&GeneralNumber=1&generalnumber1=1&GeneralCheck=on')
+page = br.open('https://www.oscn.net/applications/oscn/report.asp?report=WebJudicialDocketJudgeCaseTypeAll&errorcheck=true&Judge=1012&database=&db=Tulsa&CaseTypeID=26&StartDate=09%2F09%2F2021&GeneralNumber=1&generalnumber1=1&GeneralCheck=on')
 html = page.read().decode("utf-8")
 
 #now find each docket # and format it into an excel link
@@ -33,7 +33,10 @@ cares = []
 petitions = []
 cares = []
 petitions = []
+atty1 = []
+atty2 = []
 
+#open each docket summary URL
 for i in urls:
     #open page
     print(i)
@@ -62,11 +65,26 @@ for i in urls:
         petitions.append("https://www.oscn.net/applications/oscn/getimage.tif?submitted=true&casemasterid=" + caseID + "&db=TULSA&" + petitioncode)
         cares.append("https://www.oscn.net/applications/oscn/getimage.tif?submitted=true&casemasterid=" + caseID + "&db=TULSA&" + carescode)
 
+    #now we get the attorneys names and drop them into lists
+    atty = []
+    atty = re.findall("(?<=\t\t\t\t)(.*)(?=\(Bar # \d+)", html)
+    try:
+        atty1.append(atty[0])
+    except:
+        atty = ["none", "none"]
+        atty1.append(atty[0])
+    try:
+        atty2.append(atty[1]) 
+    except:
+        atty = ["none", "none"]
+        atty2.append(atty[1])
+    
 excelpets = ["=HYPERLINK(\"" + e + "\", \"Petition\")" for e in petitions]
 excelcares = ["=HYPERLINK(\"" + e + "\", \"Cares Verification\")" for e in cares]
 
 print("done!")
 
+#pull down images and convert to pngs
 os.chdir("C:/Users/AnthonySeverin/TIFs")
 count = 1
 
@@ -77,7 +95,7 @@ for i in petitions:
     newfilename = str(count) + ".png"
     open(filename, 'wb').write(r.content)
     
-    #conversion goes here
+    #convert to png
     im = Image.open(filename)
     for i, page in enumerate(ImageSequence.Iterator(im)): #kernel crashes unless this is in a for loop for some ungodly reason
         page.save(newfilename)
@@ -87,19 +105,28 @@ for i in petitions:
     
 print("done")
 
-
 #now we call the textract wrapper and initialize address lists
 client = boto3.client('textract')
 address1list = []
 address2list = []
-
+count = 0
 
 #open each image and read it
 while i <= count:
-    count = count - 1
+    
+    #clear variables and set count
+    address1 = []
+    address2 = []
+    count = count + 1
     filename = str(count) + ".png"
-    with open(filename, 'rb') as document:
-        img = bytearray(document.read())
+    
+    #open file
+    try:
+        with open(filename, 'rb') as document:
+            img = bytearray(document.read())
+    except:
+        print("Found all files.")
+        break
 
     #call textract
     response = client.detect_document_text(Document={'Bytes': img})
@@ -110,26 +137,34 @@ while i <= count:
         if item["BlockType"] == "LINE":
             text += "\n" + item["Text"]
 
-    #use regex to pull out addresses
-    address1 = re.findall("(?<=resides at ).*?(=%d{5})", text)
-    address2 = re.findall("(?<=address is ).*?(=%d{5})", text)
+    #use regex to pull out addresses on the next line
+    address1 = re.findall("(?<=address is\n).*", text)
+    address2 = re.findall("(?<=resides at\n).*", text)
     
+    #if that fails, try looking on the same line
+    if address1 == []:
+        address1 = re.findall("(?<=resides at).*", text)
+
+    if address2 == []:
+        address2 = re.findall("(?<=address is).*", text)
+
     print(count)
     print(address1)
     print(address2)
-    print(text)
     
     #append to addresslists
     address1list.append(address1)
     address2list.append(address2)
     
-    #send to dataframes so pandas can export to csv
+#send to dataframes so pandas can export to csv
 df = pd.DataFrame()
 df["Docket Number"] = dockets
 df["Docket Links"] = excellinks
 df["Petition"] = excelpets
 df["Cares Act Affidavit"] = excelcares
-#df["ResAdd"] = address1list
-#df["MailAdd"] = address2list
+df["ResAdd"] = address1list
+df["MailAdd"] = address2list
+df["Atty1"] = atty1
+df["Atty2"] = atty2
 
 df.to_csv('evictions.csv', index=False)
