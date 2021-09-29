99e890c2a9854a7c715521995c3b261444fbb042
apologet1c <42360331+apologet1c@users.noreply.github.com>
2021-09-29 15:01:44 -0500

    Update TulsaScraper.py
    Added an input feature, commented, cleaned up code

--- TulsaScraper.py
+++ TulsaScraper.py
@@ -1,60 +1,69 @@
 import mechanize
-import urllib.request
 import pandas as pd
-import string
 import re
 import os
 import requests
 import boto3
 from PIL import Image, ImageSequence
 
-#can't remember what this does
-pd.options.mode.chained_assignment = None
+pd.options.mode.chained_assignment = None #stop pandas from throwing errors
 
 br = mechanize.Browser()
 br.set_handle_robots(False)   # ignore robots.txt
 br.set_handle_refresh(False)  # can sometimes hang without this
 
-#get the webpage
-page = br.open('https://www.oscn.net/applications/oscn/report.asp?report=WebJudicialDocketJudgeCaseTypeAll&errorcheck=true&Judge=1012&database=&db=Tulsa&CaseTypeID=26&StartDate=09%2F21%2F2021&GeneralNumber=1&generalnumber1=1&GeneralCheck=on')
+
+
+#PART 1: SETUP
+#get url from user
+docketurl = input("What is the docket URL? ") #example input: https://www.oscn.net/applications/oscn/report.asp?report=WebJudicialDocketJudgeCaseTypeAll&errorcheck=true&Judge=1012&database=&db=Tulsa&CaseTypeID=26&StartDate=09%2F30%2F2021&GeneralNumber=1&generalnumber1=1&GeneralCheck=on
+
+#get the docket list
+page = br.open(docketurl)
 html = page.read().decode("utf-8")
 
-#now find each docket # and format it into an excel link
-dockets = re.findall("SC-\d+-\d+", html)
+#now generate links to every case on the docket and add to a urls list
 snippets = re.findall("GetCaseInformation.asp\?submitted=true&db=Tulsa&casemasterid=\d+", html)
 urls = ["https://www.oscn.net/applications/oscn/" + e for e in snippets]
 
+
+
+#PART 2: SCRAPING LINKS
 #initialize all lists
-addresses = []
+docketnums = []
 captions = []
-times = []
-cares = []
+url1 = []
 petitions = []
 cares = []
-petitions = []
 atty1 = []
 atty2 = []
-url1 = []
 
 #open each docket summary URL
 for i in urls:
-    #open page
+    
     try:
+        #open page
         page = br.open(i)
         html = page.read().decode("utf-8", errors='ignore')
         
-        #checks to see if it's an FED
+        #test to see if it's an FED
         match = re.search("FORCIBLE ENTRY", html)
         testfed = bool(match)
 
     except:
-        print("dropping")
+        #we don't want it in the list if not FED or if link doesn't exist
+        print("dropping") 
     
+    #only add the FEDs to list url1
     if testfed is True:
         url1.append(i)
     else:
         print("dropping")
         continue
+    
+    #now we get the docket number and add to a list
+    docketnum = re.findall("SC-\d+-\d+", html)[0]
+    docketnums.append(docketnum)
     
     #now we get the case caption
     caption = ""
@@ -71,21 +80,21 @@ for i in urls:
         petitions.append("NONE")
         cares.append("NONE")
 
-    #what if we find one document
+    #what if we find one document?
     if len(barcodes) == 1:
         petitioncode = barcodes[0]
         carescode = "NONE"
         petitions.append("https://www.oscn.net/applications/oscn/getimage.tif?submitted=true&casemasterid=" + caseID + "&db=TULSA&" + petitioncode)
         cares.append("NONE")
 
-    #what if we find 2+ documents
+    #what if we find 2+ documents?
     if len(barcodes) >= 2:
         petitioncode = barcodes[0]
         carescode = barcodes[1]
         petitions.append("https://www.oscn.net/applications/oscn/getimage.tif?submitted=true&casemasterid=" + caseID + "&db=TULSA&" + petitioncode)
         cares.append("https://www.oscn.net/applications/oscn/getimage.tif?submitted=true&casemasterid=" + caseID + "&db=TULSA&" + carescode)
 
-    #now we get the attorney names and drop them into lists
+    #now we get the attorneys who have registered appearances and drop the names into lists
     atty = []
     atty = re.findall("(?<=\t\t\t\t)(.*)(?=\(Bar # \d+)", html)
     
@@ -96,44 +105,51 @@ for i in urls:
         atty = [" ", " "]
         atty1.append(atty[0])
     
-    #tries to append second name
+    #tries to append a second name
     try:
         atty2.append(atty[1]) 
     except:
         atty = [" ", " "]
         atty2.append(atty[1])
 
-#reformats the urls into an excel hyperlink
+#reformats the urls into excel hyperlinks
+excellinks = ["=HYPERLINK(\"" + e + "\", \"Summary\")" for e in url1]
 excelpets = ["=HYPERLINK(\"" + e + "\", \"Petition\")" for e in petitions]
 excelcares = ["=HYPERLINK(\"" + e + "\", \"Cares Verification\")" for e in cares]
 
 #manually check if all match
+print(len(docketnums))
 print(len(url1))
 print(len(petitions))
 print(len(cares))
-print("done!")
 
+
+
+#PART 3: DOWNLOAD IMAGE FILES OF THE PETITIONS
 #set download folder
 os.chdir("C:/Users/AnthonySeverin/TIFs")
 count = 1
 
 for i in petitions:  
-    #pulls down tif file from OSCN
+    #pulls down tif files from OSCN
     r = requests.get(i, allow_redirects=True)
-    
     filename = str(count) + ".tif"
     newfilename = str(count) + ".png"
     open(filename, 'wb').write(r.content)
     
-    #conversion goes here
+    #convert from tif to pdf
     im = Image.open(filename)
-    for i, page in enumerate(ImageSequence.Iterator(im)): #kernel crashes unless this is in a for loop for some ungodly reason
+    for i, page in enumerate(ImageSequence.Iterator(im)): #kernel crashes unless it's done this way for some ungodly reason
         page.save(newfilename)
         break
     #increment counter for filenames
     count = count + 1
-    
+    print(count)
 print("done")
+
+
+
+#PART 4: OCR PETITION AND RECOGNIZE ADDRESSES
 
 #now we call the textract wrapper and initialize address lists
 client = boto3.client('textract')
@@ -189,14 +205,16 @@ while i <= count:
     #print(address1)
     #print(address2)
     #print(text)
-    
-#send to dataframes so pandas can export to csv
-excellinks = ["=HYPERLINK(\"" + e + "\", \"Summary\")" for e in url1]
 
+ 
+    
+#PART 5: EXPORT TO EXCEL
+
+#send to dataframes so pandas can export to csv
 df = pd.DataFrame()
 
 #set up columns and writes from variables
-df["Docket Number"] = dockets
+df["Docket Number"] = docketnums
 df["Captions"] = captions
 df["Docket Links"] = excellinks
 df["Petition"] = excelpets
@@ -206,4 +224,4 @@ df["MailAdd"] = address2list
 df["Atty1"] = atty1
 df["Atty2"] = atty2
 
-df.to_csv('evictions.csv', index=False)
+df.to_csv('evictions.csv', index=False) #look in the TIFs folder
