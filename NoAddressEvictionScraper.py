100c27ca8cbf8c5aad384e983c56171402b7b33d
apologet1c <42360331+apologet1c@users.noreply.github.com>
2023-01-09 09:44:44 -0600

    Update NoAddressEvictionScraper.py
    Adds captcha detection

--- NoAddressEvictionScraper.py
+++ NoAddressEvictionScraper.py
@@ -35,59 +35,82 @@ allmoney = []
 count = 0
 
 # open each docket summary URL
+
+# open each docket summary URL
 for i in urls:
+
     count = count + 1
     try:
         # open page
         page = br.open(i)
         html = page.read().decode("utf-8", errors='ignore')
 
-        # test to see if it's an FED
-        match = re.search("FORCIBLE ENTRY", html)
-        testfed = bool(match)
+        match = re.search("captcha", html)
+        testcaptcha = bool(match)
 
     except:
         # we don't want it in the list if not FED or if link doesn't exist
-        print("something is broken")
+        print("Something is wrong: check your internet connection?")
 
         # only add the FEDs to list url1
+    if testcaptcha is True:
+        input("Captcha Detected. Please open browser, open a docket sheet on OSCN and do a captcha.")
+
+        #try again
+        try:
+            page = br.open(i)
+            html = page.read().decode("utf-8", errors='ignore')
+        except:
+            print("Something is wrong: check your internet connection?")
+
+    # test to see if it's an FED
+    match = re.search("FORCIBLE ENTRY", html)
+    testfed = bool(match)
+
     if testfed is True:
         url1.append(i)
     else:
         print("dropped non-FED case")
         continue
 
-    # now we get the docket number and add to a list
-    docketnum = re.findall("..-20\d+-\d+", html)[0]
-    docketnums.append(docketnum)
-    print(docketnum)
+        # now we get the docket number and add to a list
+    docketnum = re.findall("..-20\d+-\d+", html)
 
-    # now we get the case caption
-    caption = re.findall("..-20\d+-\d+- [\r\n]+([^\r\n]+)", html)
+    if docketnum == []:
+        docketnum = ['captcha']
+
+    docketnums.append(docketnum[0])
+    print(docketnum[0])
 
     # split out plaintiff and defendant
-    plaintiff = re.findall("^\t(.*)\sv.\s", caption[0])
-    defendant = re.findall("\sv.\s(.*)$", caption[0])
+    plaintiff = re.findall("(?<=td valign=\"center\" width=\"50%\">)(.*)(?=,)", html)
+    defendant = re.findall("v.<br />[\r\n]+([^\r\n]+)(.*)(?=,)", html)  # looks for the v. and then looks on the next line
+
+    if plaintiff == []:
+        plaintiff = ['.']
+    if defendant == []:
+        defendant = ['.']
+
     plaintiffs.append(plaintiff[0])
     defendants.append(defendant[0])
 
-    money = re.findall("(?<=AMOUNT IN DEBT OF ).*", html)
+    # now we get the amount sued for
+    money = re.findall("(?<=AMOUNT IN DEBT OF )(.*)(?= \+)", html)
     if money == []:
-        money = ['.']
-    money = money[0]
-    money1 = re.findall("(\d\d+).*", money)
-    if money1 == []:
-        money1 = ['.']
-    allmoney.append(money1[0])
+        money = re.findall("(?<=AMOUNT IN DEBT OF)(.*)(?= POSS)", html) #captures the weird <..$..> that some clerks use
+    if money == []:
+        money = ['.'] #if nothing is found at all
 
-    #get file links, which are in the form of getimage.tif\?submitted=true&casemasterid=\d+&db=.*&barcode=\d+
-    barcodelinks = re.findall("getimage\.tif\?submitted=true&amp;casemasterid=\d+&amp;db=.*&amp;barcode=\d+", html)
+    allmoney.append(money[0])
 
-    #remove amp; from barcodelinks
+    # get barcodes that link to the specific files.
+    barcodelinks = re.findall("(&bc=\d+&fmt=tif)", html)
+
+    # remove amp; from barcodelinks
     barcodelinks = [e.replace("amp;", "") for e in barcodelinks]
 
-    #add https://www.oscn.net/applications/oscn/ to each link
-    barcodelinks = ["https://www.oscn.net/applications/oscn/" + e for e in barcodelinks]
+    # append the barcode to the end of every link
+    barcodelinks = ["https://www.oscn.net/dockets/GetDocument.aspx?ct=tulsa" + e for e in barcodelinks]
 
     # what if we don't find any documents?
     if len(barcodelinks) == 0:
@@ -107,8 +130,9 @@ for i in urls:
 
     # now we get the attorneys who have registered appearances and drop the names into lists
     atty = []
-    atty = re.findall("(?<=\t\t\t\t)(.*)(?=\(Bar # \d+)", html)
+    atty = re.findall("(?<=\<td valign=\"top\" width=\"50%\"\>)(.*)(?=,&nbsp;)", html)
 
+    # (?<=<td valign=\"top\" width=\"50%\">) #(?!Bar #\d+)
     # tries to append first name
     try:
         atty1.append(atty[0])
@@ -123,14 +147,12 @@ for i in urls:
         atty = [" ", " "]
         atty2.append(atty[1])
 
-    print(count)
-
-print("done")
+print("Done with dockets.")
 
 #reformats the urls into excel hyperlinks
 excellinks = ["=HYPERLINK(\"" + e + "\", \"Summary\")" for e in url1]
 excelpets = ["=HYPERLINK(\"" + e + "\", \"Petition\")" for e in petitions]
-excelcares = ["=HYPERLINK(\"" + e + "\", \"Cares Verification\")" for e in cares]
+excelcares = ["=HYPERLINK(\"" + e + "\", \"NTQ\")" for e in cares]
 
 #manually check if all match
 print("Done. Found the following number of FED cases:")
