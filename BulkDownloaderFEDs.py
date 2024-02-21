d3f8450bf1dccb1d2408bff769a3477adc1fa5fa
apologet1c <42360331+apologet1c@users.noreply.github.com>
2024-02-21 10:44:06 -0600

    Update BulkDownloaderFEDs.py

--- BulkDownloaderFEDs.py
+++ BulkDownloaderFEDs.py
@@ -3,8 +3,8 @@ import time
 import os
 
 current_directory = os.getcwd()
-tifs_directory = current_directory + "\\SC-23"
-os.chdir(tifs_directory)
+save_directory = current_directory + "\\SC-23"
+os.chdir(save_directory)
 
 header1 = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8', 'Accept-Language': 'en-US,en;q=0.5' }
 header2 = { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.5 Safari/605.1.15', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8', 'Accept-Language': 'en-US,en;q=0.5'}
